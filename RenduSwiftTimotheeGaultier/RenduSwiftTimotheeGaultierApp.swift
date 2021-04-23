1d3f6f06a8f92217cfcffa984a348827d9e98e2f
Timothée Gaultier <timothee.gaultier@gmail.com>
2021-04-23 07:59:23 +0200

    Initial Commit

---
+++ RenduSwiftTimotheeGaultier/RenduSwiftTimotheeGaultierApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenduSwiftTimotheeGaultierApp.swift
+//  RenduSwiftTimotheeGaultier
+//
+//  Created by timogo on 23/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct RenduSwiftTimotheeGaultierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
